c0470ef371a22986a45d9c5b4333250c240b2ddb
Muvunyi <muvunyiiddy@gmail.com>
2020-08-23 17:07:50 +0200

    name edit

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import {
   Navbar,
-  NavbarBrand,
   Jumbotron,
   Nav,
   NavbarToggler,
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,6 @@ import {
   Card,
   CardBody,
   CardImg,
-  CardImgOverlay,
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
